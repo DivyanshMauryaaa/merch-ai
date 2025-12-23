a7adb8c5691781f4975e7c54bb2760b3260af4be
divyanshMauryaaa <divyanshmt510@gmail.com>
2025-12-23 15:48:45 +0530

    Big UPDATE!!

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -90,7 +90,7 @@ export default function Pricing() {
                                 }
               `}
                         >
-                            Get Started
+                            Coming soon
                         </button>
                     </ScrollAnimation>
                 ))}
